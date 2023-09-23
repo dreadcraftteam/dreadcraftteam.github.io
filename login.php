99b14ad78a0a149b1dace82c53e9e72143614b89
dreadcraftteam <136819809+dreadcraftteam@users.noreply.github.com>
2023-09-23 20:33:08 +0500

    Add files via upload

---
+++ login.php
@@ -0,0 +1,104 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+
+
+
+
+
+
+<html lang="en">
+
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="SHORTCUT ICON" href="favicon.png"/>
+<title>DREADCRAFT TEAM</title>
+<link rel="stylesheet" type="text/css" href="style2.css">
+<script type="text/javascript" src="https://gc.kis.v2.scr.kaspersky-labs.com/FD126C42-EBFA-4E12-B309-BB3FDD723AC1/main.js?attr=vAGi6HPQWfH6j84d4HeQc0vO1wolQegQYPymaO3j28pa_mGd5Hts9xWLnU3hdTm1mJQj4Cv47a3pYyn71DriTw" charset="UTF-8"></script></head>
+<body>
+
+<div id="centerpage">
+
+<div id="toppanel" style="z-index:3;">
+
+<div class="top_center" style="height:48px; width:100%;">
+<table style="height:48px; width:100%;">
+	<tr>
+    	<td style="width:270px; height: 50px; padding:0px; top:0px;">
+        
+<div id="prepurchasesplash" style="height: 88px;">        
+        
+         
+<!--<a href="/web/20110102061758/http://www.minecraft.net/prepurchase.jsp" border="0">--><img src="images/logos/logo_main_small.png" width="220" height="42" border="0"></a>
+        
+        
+ </div>       
+        <td>
+        <div id="links">
+		
+			<a href="index.html" id="menu_" class="menuItem">Home</a>&nbsp; &nbsp;
+			<a href="Comingsoon.html" id="menu_about" class="menuItem">Play Demo</a> &nbsp; &nbsp;
+		        <a href="Comingsoon.html" id="menu_" class="menuItem">Alpha+ Archive</a> &nbsp; &nbsp;
+			<a href="https://discord.gg/nMNB7hsa7A" id="menu_help" class="menuItem">Discord</a>&nbsp; &nbsp;
+		
+        </div>
+        </td>
+
+        <td>
+        
+        <div id="loginpanel">
+  <a href="https://www.sithgames.com/login/" class="loginpanel">Log in</a> I <a href="https://www.sithgames.com/register/" class="loginpanel">Register</a>  
+</div>
+        
+        
+        </td>
+    </tr>
+</table>
+</div>
+<a href="Comingsoon.html">Donate!</a>
+</div>
+        
+<div id="toppanel_bg" style="z-index:1;"></div>
+
+<div id="page" style="z-index:4;">
+
+<div id="content">
+<br>
+<br>
+<br>
+
+
+<h2>Login</h2>
+
+<form name="input" action="login.php" method="post">
+<table>
+		<tbody><tr>
+		<td>Username:</td>
+		<td><input type="text" name="username" value=""></td>
+	</tr>
+	<tr>
+		<td>Password:</td>
+		<td><input type="password" name="password"></td>
+	</tr>
+	<tr>
+		<td></td>
+		<td><input type="submit" value="Log in!"></td>
+	</tr>
+</tbody></table>
+<br>
+<a href="register.php">Register an account</a><br>
+<a href="forgotpass.php">Forgot password?</a></form>
+
+</div>
+<strong>DREADCRAFT TEAM 2020-<script>document.write(new Date().getFullYear())</script></strong>
+<br>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-4CCVDC3NBN"></script>
+<script>
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
+	
+	gtag('config', 'G-4CCVDC3NBN');
+</script>
+</body>
+</html>
